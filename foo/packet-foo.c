29b39b6830040b75e49050fe244474a69cb942ac
gnlsw <today_wsl@163.com>
2017-11-29 22:17:41 +0800

    详细解析各字段

--- foo/packet-foo.c
+++ foo/packet-foo.c
@@ -5,14 +5,31 @@
 
 static int proto_foo = -1;
 
+static int hf_foo_pdu_type = -1;
+static int hf_foo_flags = -1;
+static int hf_foo_sequenceno = -1;
+static int hf_foo_initialip = -1;
+static int ett_foo = -1;
+
 static int
 dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
+    gint offset = 0;
+
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "FOO");
     /* clear out stuff in the info column */
     col_clear(pinfo->cinfo, COL_INFO);
 
-    (void)proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_NA);
+    proto_item *ti = proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_NA);
+    proto_tree *foo_tree = proto_item_add_subtree(ti, ett_foo);
+    proto_tree_add_item(foo_tree, hf_foo_pdu_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+    offset += 1;
+    proto_tree_add_item(foo_tree, hf_foo_flags, tvb, offset, 1, ENC_BIG_ENDIAN);
+    offset += 1;
+    proto_tree_add_item(foo_tree, hf_foo_sequenceno, tvb, offset, 2, ENC_BIG_ENDIAN);
+    offset += 2;
+    proto_tree_add_item(foo_tree, hf_foo_initialip, tvb, offset, 4, ENC_BIG_ENDIAN);
+    offset += 4;
 
     return tvb_captured_length(tvb);
 }
@@ -20,11 +37,46 @@ dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data 
 void
 proto_register_foo(void)
 {
+    static hf_register_info hf[] = {
+        { &hf_foo_pdu_type,
+            {"FOO PDU Type", "foo.type",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL}
+        },
+        { &hf_foo_flags,
+            {"FOO PDU Flags", "foo.flags",
+            FT_UINT8, BASE_HEX,
+            NULL, 0x0,
+            NULL, HFILL}
+        },
+        { &hf_foo_sequenceno,
+            {"FOO PDU Sequence Number", "foo.seqn",
+            FT_UINT16, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL}
+        },
+        { &hf_foo_initialip,
+            {"FOO PDU Initial IP", "foo.initialip",
+            FT_IPv4, BASE_NONE,
+            NULL, 0x0,
+            NULL, HFILL}
+        },
+    };
+
+    /* Setup protocol sbutree array */
+    static gint *ett[] = {
+        &ett_foo
+    };
+
     proto_foo = proto_register_protocol(
         "FOO Protocol", /* name */
         "FOO", /* short name */
         "foo" /* abbrev */
         );
+
+    proto_register_field_array(proto_foo, hf, array_length(hf));
+    proto_register_subtree_array(ett, array_length(ett));
 }
 
 void
